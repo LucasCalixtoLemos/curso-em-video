137a469109c4ad0d97c94cbd8f4d01536e693bba
LucasCalixtoLemos <lucas18calixto@gmail.com>
2026-03-02 18:10:02 -0300

    Desafio 001 resolvido

---
+++ Desafios/001.py
@@ -0,0 +1,5 @@
+#Crie um script que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.
+
+nome = input('Qual o seu nome?: ')
+#print('Olá', nome, '! Prazer em te conhecer!')
+print('Olá {}! Prazer em te conhecer!'.format(nome))
