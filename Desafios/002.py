8338023831a1ea3706ae18f8519dd186431bbe2c
LucasCalixtoLemos <lucas18calixto@gmail.com>
2026-03-02 18:10:32 -0300

    Desafio 002 resolvido

---
+++ Desafios/002.py
@@ -0,0 +1,7 @@
+#Crie um script que leia o dia, o mês e o ano de uma pessoa, e mostre uma mensagem com a data formatada
+
+dia = input('Que dia você nasceu?: ')
+mes = input('Que mês você nasceu?: ')
+ano = input('Que ano você nasceu?: ')
+#print('{}/{}/{}'.format(dia, mes, ano))
+print('Você nasceu no dia {} do {} de {}. Correto?'.format(dia, mes, ano))
