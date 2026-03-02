7db3b6d921b60a52cfe9de32b415217224d17dbb
LucasCalixtoLemos <lucas18calixto@gmail.com>
2026-03-02 18:10:44 -0300

    Desafio 003 metade resolvido

---
+++ Desafios/003.py
@@ -0,0 +1,5 @@
+#Crie um script que leia 2 números e mostre a soma entre eles.
+
+numero1 = input('Escolha um número: ')
+numero2 = input('Escolha outro número: ')
+print('A soma entre eles é:', numero1 + numero2)
