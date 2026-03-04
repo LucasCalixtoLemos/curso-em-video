6118d1019601ea9e23df5dd277c958c0d7e02fc6
LucasCalixtoLemos <lucas18calixto@gmail.com>
2026-03-04 15:36:27 -0300

    apliquei o tipo primitivo int

--- Desafios/003.py
+++ Desafios/003.py
@@ -1,5 +1,6 @@
 #Crie um script que leia 2 números e mostre a soma entre eles.
 
-numero1 = input('Escolha um número: ')
-numero2 = input('Escolha outro número: ')
-print('A soma entre eles é:', numero1 + numero2)
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro: '))
+s = n1 + n2
+print('A soma dos números é', s)
